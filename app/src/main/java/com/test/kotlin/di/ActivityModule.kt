1c33ad1f6f5531dd851315f229d3649938435b45
Aleksandr Khomenko <madhoma@proglife.com.ua>
2019-05-13 16:43:15 +0300

    change

--- app/src/main/java/com/test/kotlin/di/ActivityModule.kt
+++ app/src/main/java/com/test/kotlin/di/ActivityModule.kt
@@ -1,8 +1,5 @@
 package com.test.kotlin.di
 
-import com.test.kotlin.presentation.base.model.BaseModel
-import org.koin.android.ext.koin.androidApplication
-import org.koin.dsl.module.applicationContext
 import org.koin.dsl.module.module
 
 object ActivityModule {
--- app/src/main/java/com/test/kotlin/mvp/view/MainView.kt
+++ app/src/main/java/com/test/kotlin/mvp/view/MainView.kt
@@ -1,7 +1,6 @@
 package com.test.kotlin.mvp.view
 
 import com.test.kotlin.presentation.base.view.BaseView
-import com.test.kotlin.presentation.base.view.Presenter
 
 interface MainView: BaseView {
     fun showId(mId: Long)
